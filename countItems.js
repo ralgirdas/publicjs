81f25fcc61c48f8ca2a83fdff63cf99c3f660b23
ralgirdas <algirdas.rabikauskas@gmail.com>
2013-08-30 16:32:00 +0300

    Update countItems.js

--- countItems.js
+++ countItems.js
@@ -20,7 +20,7 @@
     	//Go through products, collect unique soldouts
     	var products = $('.product').each(function(){ 
 	var $this = $(this); 
-        if($this.find('.fsem1-7').length > 0 && $this.context.innerText.indexOf('Sold Out') > 0){
+        if($this.find('.fsem1-7').length > 0 /*&& $this.context.innerText.indexOf('Sold Out') > 0*/){
         	soldOut++;
         }
 	});
